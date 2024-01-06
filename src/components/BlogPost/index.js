51025e6c7925a86afc7d32d97c5700a0f5d90ca4
wunnle <me@wunnle.com>
2024-01-06 19:25:59 +0300

    added seo tags for posts

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -2,14 +2,35 @@ import timeSince from '@/utils/time-since'
 import styles from './BlogPost.module.css'
 import Markdown from 'react-markdown'
 import Webmentions from '@/components/Webmentions'
+import Head from 'next/head'
 
 const BlogPost = ({ post }) => {
   const { meta, content } = post
 
-  const { title, date } = meta
+  const { title, date, featuredImg } = meta
 
   return (
     <div>
+      <Head>
+        <title>{title} | Mostly Indie</title>
+        <meta name="description" content={title} />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="article" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@mostlyindie" />
+        <meta name="twitter:creator" content="@mostlyindie" />
+        <meta property="og:url" content={`https://mostlyindie.com/${post.slug}`} />
+        <meta property="og:description" content={title} />
+        <meta
+          property="og:image"
+          content={`/images/articles/${post.slug}/${featuredImg.replace('./', '')}`}
+        />
+        <meta
+          property="twitter:image"
+          content={`/images/articles/${post.slug}/${featuredImg.replace('./', '')}`}
+        />
+        <link rel="canonical" href={`https://mostlyindie.com/${post.slug}`} />
+      </Head>
       <div className={styles.post}>
         <h1 className={[styles.title, 'p-name'].join(' ')}>{title}</h1>
         <div style={{ display: 'none' }}>
