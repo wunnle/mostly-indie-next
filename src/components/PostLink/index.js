a8a96861ed6121106b0953b014d437fb146e8163
wunnle <me@wunnle.com>
2024-01-06 18:52:12 +0300

    cleaned up postLink

--- src/components/PostLink/index.js
+++ src/components/PostLink/index.js
@@ -1,8 +1,7 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './PostLink.module.css'
 import timeSince from '@/utils/time-since'
+import Link from 'next/link'
 import Markdown from 'react-markdown'
+import styles from './PostLink.module.css'
 
 const PostLink = ({ post }) => {
   const { slug, meta } = post
@@ -21,13 +20,6 @@ const PostLink = ({ post }) => {
           }}
           className={styles.postImg}
         ></div>
-        // <Image
-        //   className={styles.postImg}
-        //   src={`/images/articles/${slug}/${featuredImg.replace('./', '')}`}
-        //   alt="Alba"
-        //   width={300}
-        //   height={300}
-        // />
       )}
       <div className={styles.postInfo}>
         <div className={styles.postTitle}>
