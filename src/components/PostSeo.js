d31d5e0819ddfbb3c19438073185ca11049dcda9
wunnle <me@wunnle.com>
2025-07-25 19:26:01 +0300

    removed unnecessary logs

--- src/components/PostSeo.js
+++ src/components/PostSeo.js
@@ -19,18 +19,23 @@ const PostSeo = ({ post }) => {
       <meta name="twitter:creator" content="@mostlyindie" />
       <meta property="og:url" content={`https://mostlyindie.com/${post.slug}`} />
       <meta property="og:description" content={summary} />
-      <meta
-        property="og:image"
-        content={`https://mostlyindie.com/images/articles/${
-          post.slug
-        }/${featuredImg.replace('./', '')}`}
-      />
-      <meta
-        property="twitter:image"
-        content={`https://mostlyindie.com/images/articles/${
-          post.slug
-        }/${featuredImg.replace('./', '')}`}
-      />
+
+      {featuredImg && (
+        <>
+          <meta
+            property="og:image"
+            content={`https://mostlyindie.com/images/articles/${
+              post.slug
+            }/${featuredImg.replace('./', '')}`}
+          />
+          <meta
+            property="twitter:image"
+            content={`https://mostlyindie.com/images/articles/${
+              post.slug
+            }/${featuredImg.replace('./', '')}`}
+          />
+        </>
+      )}
       <link rel="canonical" href={`https://mostlyindie.com/${post.slug}`} />
       <meta property="og:site_name" content="Mostly Indie" />
       <meta property="article:published_time" content={date} />
