a30d271643f4d9e2ec531cc53e0f4efbddf7d385
wunnle <me@wunnle.com>
2025-07-29 19:46:30 +0300

    updated about page and added about link to header

--- src/components/Tagline/index.js
+++ src/components/Tagline/index.js
@@ -2,6 +2,7 @@ import styles from './Tagline.module.css'
 import { Silkscreen } from 'next/font/google'
 import Image from 'next/image'
 import logo from './logo.png'
+import Link from 'next/link'
 
 const silkscreen = Silkscreen({ subsets: ['latin'], weight: ['400', '700'] })
 
@@ -10,9 +11,16 @@ const Tagline = () => (
     <div className={styles.holder}>
       <Image className={styles.logo} src={logo} alt="Mostly Indie" />
       <div className={styles.tagline}>
-        game reviews and news. but mostly about{' '}
+        Game reviews. Mostly{' '}
         <span className={[silkscreen.className, styles.indie].join(' ')}>indies</span>
       </div>
+      <Link
+        href="/about"
+        className={['h-card', silkscreen.className, styles.menuLink].join(' ')}
+        rel="me"
+      >
+        About
+      </Link>
     </div>
   </div>
 )
